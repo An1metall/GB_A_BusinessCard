0118e28f418ae6db7e3e1efbfdda33ff86bde065
Roman Bardashov <bardashovrv@gmail.com>
2016-05-25 21:51:11 +0300

    ViewHolder animation added Action availability check implemented

--- app/src/main/java/com/an1metall/businesscard/RecyclerViewAdapter.java
+++ app/src/main/java/com/an1metall/businesscard/RecyclerViewAdapter.java
@@ -1,17 +1,22 @@
 package com.an1metall.businesscard;
 
-import android.content.ActivityNotFoundException;
 import android.content.ComponentName;
 import android.content.Context;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.content.pm.ResolveInfo;
+import android.graphics.Point;
 import android.net.Uri;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.CardView;
 import android.support.v7.widget.RecyclerView;
+import android.view.Display;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.view.animation.Animation;
-import android.view.animation.AnimationUtils;
+import android.view.WindowManager;
+import android.view.animation.DecelerateInterpolator;
 import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -23,10 +28,10 @@ public class RecyclerViewAdapter extends RecyclerView.Adapter<RecyclerViewAdapte
     List<Card> cards;
 
     private int lastPosition = -1;
-    private Context context;
+    private AppCompatActivity activity;
 
-    RecyclerViewAdapter(Context context, List<Card> cards) {
-        this.context = context;
+    RecyclerViewAdapter(AppCompatActivity activity, List<Card> cards) {
+        this.activity = activity;
         this.cards = cards;
     }
 
@@ -54,12 +59,29 @@ public class RecyclerViewAdapter extends RecyclerView.Adapter<RecyclerViewAdapte
         super.onAttachedToRecyclerView(recyclerView);
     }
 
-    private void setAnimation(View viewToAnimate, int position) {
+    private void setAnimation(View view, int position) {
         if (position > lastPosition) {
-            Animation animation = AnimationUtils.loadAnimation(context, android.R.anim.slide_in_left);
-            viewToAnimate.startAnimation(animation);
             lastPosition = position;
+
+            view.setTranslationY(getScreenHeight(activity));
+            view.animate()
+                    .translationY(0)
+                    .setInterpolator(new DecelerateInterpolator(3.f))
+                    .setDuration(700)
+                    .start();
         }
+    }
+
+    private static int getScreenHeight(Context c) {
+        int screenHeight = 0;
+        if (screenHeight == 0) {
+            WindowManager wm = (WindowManager) c.getSystemService(Context.WINDOW_SERVICE);
+            Display display = wm.getDefaultDisplay();
+            Point size = new Point();
+            display.getSize(size);
+            screenHeight = size.y;
+        }
+        return screenHeight;
     }
 
     public class ViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
@@ -68,19 +90,22 @@ public class RecyclerViewAdapter extends RecyclerView.Adapter<RecyclerViewAdapte
         TextView cardName;
         TextView cardData;
         ImageView cardPic;
+        private Toast currentToast;
 
         public ViewHolder(View v) {
             super(v);
-            v.setOnClickListener(this);
             cardView = (CardView) itemView.findViewById(R.id.cv);
             cardName = (TextView) itemView.findViewById(R.id.card_name);
             cardData = (TextView) itemView.findViewById(R.id.card_data);
             cardPic = (ImageView) itemView.findViewById(R.id.card_pic);
+            v.setOnClickListener(this);
         }
 
         @Override
         public void onClick(View view) {
-            switch (getLayoutPosition()) {
+            int position = getLayoutPosition();
+            notifyItemChanged(position);
+            switch (position) {
                 case 1:
                     sendViaEmail(cardData.getText().toString());
                     break;
@@ -90,21 +115,44 @@ public class RecyclerViewAdapter extends RecyclerView.Adapter<RecyclerViewAdapte
             }
         }
 
-        public void sendViaEmail(String uri) {
-            Intent intent = new Intent(Intent.ACTION_SENDTO, Uri.fromParts("mailto", uri, null));
-            context.startActivity(Intent.createChooser(intent, "Send Email"));
+        private void sendViaEmail(String uri) {
+            if (isCallable(new Intent(Intent.ACTION_SENDTO).setData(Uri.parse("mailto:")))) {
+                Bundle bundle = new Bundle();
+                bundle.putString("uri", uri);
+
+                DatePickerDialogFragment dialog = new DatePickerDialogFragment();
+                dialog.setArguments(bundle);
+                dialog.show(activity.getSupportFragmentManager(), "DATE_PICKER");
+            }
+            else {
+                showToast(activity.getString(R.string.email_account));
+            }
         }
 
-        public void sendViaSkype(String uri) {
-            try {
-                Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse("skype:" + uri));
-                intent.setComponent(new ComponentName("com.skype.raider", "com.skype.raider.Main"));
-                intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
-                context.startActivity(intent);
+        private void sendViaSkype(String uri) {
+            Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse("skype:" + uri));
+            intent.setComponent(new ComponentName("com.skype.raider", "com.skype.raider.Main"));
+            intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+
+            if (isCallable(intent)) {
+                activity.startActivity(intent);
+            } else {
+                showToast(activity.getString(R.string.skype_not_found));
             }
-            catch (ActivityNotFoundException e) {
-                Toast.makeText(context, "Skype not found", Toast.LENGTH_SHORT).show();
+        }
+
+        private boolean isCallable(Intent intent) {
+            List<ResolveInfo> list = activity.getPackageManager().queryIntentActivities(intent,
+                    PackageManager.MATCH_DEFAULT_ONLY);
+            return list.size() > 0;
+        }
+
+        public void showToast(String text) {
+            if (currentToast != null) {
+                currentToast.cancel();
             }
+            currentToast = Toast.makeText(activity, text, Toast.LENGTH_SHORT);
+            currentToast.show();
         }
     }
 }
